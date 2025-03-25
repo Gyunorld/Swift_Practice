ef6e85d48e4714c309bb6aeac0609aa51e85f28f
kimmingyu <mingue640@gmail.com>
2025-03-25 16:31:37 +0900

    250325

---
+++ Baekjoon_2501_250325/Baekjoon_2501_250325/main.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map{ Int($0)! }
+
+let n = input[0]
+let k = input[1]
+var divisors = [Int]()
+
+for i in 1...Int(sqrt(Double(n))) {
+    if n % i == 0 {
+        divisors.append(i)
+        divisors.append(n / i)
+    }
+}
+
+divisors = Array(Set(divisors)).sorted(by: <)
+
+divisors.count < k ? print(0) : print(divisors[k - 1])
